5916d07d1289999db684dde1660b25dbf06ad8fe
samjwu <swm1000@hotmail.com>
2021-04-01 14:44:38 -0600

    palindrome

---
+++ Linked List Palindrome/palin.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> vals;
+        
+        while (head) {
+            vals.push_back(head->val);
+            head = head->next;
+        }
+        
+        for (int i=0; i<vals.size()/2; i++) {
+            if (vals[i] != vals[vals.size()-1-i]) {
+                return false;
+            }    
+        }
+        
+        return true;
+    }
+};
