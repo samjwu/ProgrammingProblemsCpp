c204e7037d1c32f7298e230ca182253f55c42353
samjwu <swm1000@hotmail.com>
2021-04-18 07:06:47 -0600

    rm

---
+++ Linked List Remove Nth Node From End/rm.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *a = head, *b = head, *prev;
+        
+        for (int i=0; i<n; i++) {
+            b = b->next;
+        }
+        
+        while (b) {
+            prev = a;
+            a = a->next;
+            b = b->next;
+        }
+        
+        if (!prev) {
+            return head->next;
+        }
+        
+        prev->next = prev->next->next;
+        return head;
+    }
+};
